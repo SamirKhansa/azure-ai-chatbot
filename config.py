995dbb46d20a5692519c2178e9790116ba00ade9
SamirKhansa <samirkhansa01@gmail.com>
2025-08-29 10:21:15 +0300

    Adding the connection to Cosmos DB database

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from openai import AzureOpenAI
 
-from keys import AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, AZURE_OPENAI_VERSION
-
+from keys import AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, AZURE_OPENAI_VERSION, COSMOS_ENDPOINT, COSMOS_KEY, DATABASE_NAME, CONTAINER_NAME
+from azure.cosmos import CosmosClient, PartitionKey
 
 def get_client():
     client = AzureOpenAI(
@@ -10,3 +10,14 @@ def get_client():
         azure_endpoint=AZURE_OPENAI_ENDPOINT
     )
     return client
+
+
+def get_CosmosDB():
+    client=CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
+    database = client.create_database_if_not_exists(id=DATABASE_NAME)
+    
+    return  database.create_container_if_not_exists(
+        id=CONTAINER_NAME,
+        partition_key=PartitionKey(path="/sessionId"),
+        
+    )
