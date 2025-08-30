50eb9780952af28822e610dc7fc4dbdc232a6876
SamirKhansa <samirkhansa01@gmail.com>
2025-08-30 22:18:44 +0300

    Connecting everything together, from calling functions to retreive the user history to uploding or updating user messages

--- ChatbotFunction/__init__.py
+++ ChatbotFunction/__init__.py
@@ -1,22 +1,30 @@
-from config import get_client,get_CosmosDB
+from keys import GPT4O_KEY,EMBEDDING_KEY, GPT4O_VERSION, EMBEDDING_VERSION, EMBEDDING_ENDPOINT, GPT4O_ENDPOINT
+from config import get_client, get_CosmosDB
 import azure.functions as func
 from helpers.ErrorHandling import UserMessageError, SystemError
-from Services.OpenAiServices import AiReply, ReplyToUser
-from helpers.MessageFormatter import  format_conversation, CreateNewId, manage_context, ConversationHistory
+from Services.OpenAiServices import AiReply, ReplyToUser, TextEmbedding
+from helpers.MessageFormatter import FormatConversation, CreateNewId, SummarizeLongHistory, ConversationHistory
 from Services.CosmosDbServices import SaveConversation
-import json
+from Services.CosmosDbServices import RetreiveRelevantChunks
 
 
 
-container=get_CosmosDB()
 
-client = get_client()
+ChatContainer = get_CosmosDB(container_name="Sessions", partitionKey="/sessionId")  
+EmbeddingsContainer = get_CosmosDB(container_name="Documents", partitionKey="/id")  
+
+
+GPT4oClient = get_client(KEY=GPT4O_KEY,ENDPOINT=GPT4O_ENDPOINT, VERSION=GPT4O_VERSION )
+EmbeddingClient=get_client(KEY=EMBEDDING_KEY, VERSION=EMBEDDING_VERSION, ENDPOINT=EMBEDDING_ENDPOINT)
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
+        
         req_body = req.get_json()
         user_message = req_body.get("message")
         session_id = req_body.get("sessionId")  
+
 
         if not user_message:
             return UserMessageError
@@ -26,31 +34,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         if not session_id:
             session_id = CreateNewId()
-
-
-        conversation, document_id=ConversationHistory(session_id=session_id, container=container)
-
-        conversation.append({"role": "user", "content": user_message})
-
         
-        conversation = manage_context(conversation, max_messages=10)
 
+        conversation, document_id = ConversationHistory(session_id=session_id, container=ChatContainer, user_message=user_message)
         
         
-        ai_message = AiReply(client=client, conversation=conversation)  
-
         
+        query_embedding = TextEmbedding(EmbeddingClient=EmbeddingClient, message=user_message)
+        print("0")
+        top_chunks = RetreiveRelevantChunks(embeddings_container=EmbeddingsContainer,query_embedding=query_embedding, top_k=3)
+        print("1")
+
+
+        context_prompt = "\n\n".join(top_chunks)
+        ai_message = AiReply(client=GPT4oClient, conversation=conversation, context=context_prompt)
+
         conversation.append({"role": "bot", "content": ai_message})
-
+        SaveConversation(ChatContainer, document_id, session_id, conversation)
+        conversation_history = FormatConversation(conversation)
         
-        SaveConversation(container, document_id, session_id, conversation)
-
         
-        conversation_history = format_conversation(conversation)
 
-        return ReplyToUser(ai_message,session_id, conversation_history)
-        
+        return ReplyToUser(ai_message, session_id, conversation_history)
 
     except Exception as e:
         return SystemError(e)
-
