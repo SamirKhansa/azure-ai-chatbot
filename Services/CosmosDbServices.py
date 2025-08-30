ab3aac8637c62bcac30391f43b63f99f484850e3
SamirKhansa <samirkhansa01@gmail.com>
2025-08-30 22:09:30 +0300

    Adding Functions that would save retreive relevan information from the database

--- Services/CosmosDbServices.py
+++ Services/CosmosDbServices.py
@@ -1,6 +1,18 @@
+from helpers.ChromoDbDocuments import RankingChunks
+
 def SaveConversation(container, document_id, session_id, conversation):
     container.upsert_item({
         "id": document_id,
         "sessionId": session_id,
         "messages": conversation
     })
+
+
+def RetreiveRelevantChunks(embeddings_container, query_embedding, top_k=3):
+    query = "SELECT * FROM c"
+    items = list(embeddings_container.query_items(
+        query=query,
+        enable_cross_partition_query=True  
+    ))
+
+    return RankingChunks(query_embedding=query_embedding, top_k=top_k, items=items)
