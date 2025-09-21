c8f605b77acd1aabea6ee9eca6c0194f4549dde2
SamirKhansa <samirkhansa01@gmail.com>
2025-09-21 17:07:31 +0300

    Fixing an error in extract text with read

--- Services/DocumentIntelligence.py
+++ Services/DocumentIntelligence.py
@@ -1,12 +1,13 @@
-def extract_text_with_read(DIClient, file_path: str, locale: str | None = None) -> str:
-    with open(file_path, "rb") as f:
-        poller = DIClient.begin_analyze_document(
-            model_id="prebuilt-read",
-            body=f,
-            locale=locale,          
-        )
-    result = poller.result()
-    
-    return result.content or ""
+import io
 
-# document_text = extract_text_with_read(DIClient,"../Documents/PDF Documents/Chess opening.pdf", locale="en")
+def extract_text_with_read(DIClient, uploaded_file: bytes, locale: str | None = None) -> str:
+    file_stream = io.BytesIO(uploaded_file)
+
+    poller = DIClient.begin_analyze_document(
+        model_id="prebuilt-read",
+        body=file_stream,
+        locale=locale,          
+    )
+    
+    result = poller.result()
+    return result.content or ""
