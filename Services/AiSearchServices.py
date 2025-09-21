e92aa8157fbbaad96ed1e9f70c77c260cfafdd2e
SamirKhansa <samirkhansa01@gmail.com>
2025-09-21 17:08:52 +0300

    Adding features by the admin updating, removing features in Ai search

--- Services/AiSearchServices.py
+++ Services/AiSearchServices.py
@@ -15,16 +15,34 @@ def RetreiveRelevantChunks(search_client, query_embedding, top_k=3):
     return list(results)
 
 
-def UploadingDocuments(search_client, chunks, embeddings, Resource):
+def UploadingDocumentsAiSearch(search_client, chunks, embeddings, Resource,Type, UploadedBy, DocumentName):
     search_documents = []
     for i, chunk in enumerate(chunks):
         doc = {
             "id": str(i),  # unique ID per chunk
             "Content": chunk, 
-            "resource": Resource , # the text chunk
+            "resource": Resource ,
+            "Type":Type,
+            "UploadedBy":UploadedBy,
+            "Name":DocumentName,
             "Embeddings": embeddings[i]  # corresponding embedding
             
         }
         search_documents.append(doc)
     return search_client.upload_documents(documents=search_documents)
+
+
+def DeletingDocument(req_body, search_client):
+    
+    resource=req_body.get("resource")
+    results = search_client.search(
+        search_text="*",
+        filter=f"resource eq '{resource}'"
+    )
+    doc_ids_to_delete = [doc['id'] for doc in results]
+    if doc_ids_to_delete:
+        search_client.delete_documents(documents=[{"id": id} for id in doc_ids_to_delete])
+    return len(doc_ids_to_delete)
+    
+
     
