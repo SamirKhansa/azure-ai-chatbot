0b2d81d24f8a96cba28726123aee4ba3ec3aae30
SamirKhansa <samirkhansa01@gmail.com>
2025-09-21 17:12:08 +0300

    Implementing the chatting backend in this folder which is used to call multiple functions

---
+++ Controllers/Chat.py
@@ -0,0 +1,65 @@
+
+
+from helpers.ErrorHandling import UserMessageError
+from Services.OpenAiServices import AiReply, SendReply, TextEmbedding
+from helpers.MessageFormatter import FormatConversation, CreateNewId, AddingRefrencesToAiMessage
+from Services.CosmosDbServices import SaveConversation, ConversationHistory
+from Services.AiSearchServices import RetreiveRelevantChunks
+from Services.SpeechAPI import text_to_speech, speech_to_text
+
+
+
+
+
+
+
+
+
+def ChatBot(req_body,EmbeddingClient, GPT4oClient, speechClient, search_client, ChatContainer):
+    
+    
+    user_message = req_body.get("message")
+    audio_base64 = req_body.get("audioBase64")
+    session_id = req_body.get("sessionId")
+    
+    if audio_base64:
+        user_message=speech_to_text(audio_base64, speechClient)
+        
+    if not user_message:
+        return UserMessageError("Please provide a 'message' in JSON",400)
+
+    if user_message.strip().lower() in ['exit', 'quit', 'bye']:
+        return SendReply("Goodbye! Have a nice day 🙂")
+
+
+    if not session_id:
+        session_id = CreateNewId()
+    
+
+    conversation, document_id = ConversationHistory(session_id=session_id, container=ChatContainer, user_message=user_message)
+    
+    
+    
+    query_embedding = TextEmbedding(EmbeddingClient=EmbeddingClient, message=user_message)
+    
+    results = RetreiveRelevantChunks(search_client=search_client,query_embedding=query_embedding, top_k=3)
+    
+    
+    retrieved_texts = [result["Content"] for result in results]
+    
+    context_prompt = "\n\n".join(retrieved_texts)
+    
+
+    ai_message = AiReply(client=GPT4oClient, conversation=conversation, context=context_prompt)
+    audio_base64=text_to_speech(ai_message, speechClient)
+    
+    ai_message=AddingRefrencesToAiMessage(results=results, ai_message=ai_message)
+    
+    
+    conversation.append({"role": "bot", "Content": ai_message})
+    
+    SaveConversation(ChatContainer, document_id, session_id, conversation)
+    
+    conversation_history = FormatConversation(conversation)
+        
+    return SendReply(ai_message, session_id, conversation_history,audio_base64)
