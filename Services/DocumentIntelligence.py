7ae7de87b4108d8d7265ca9f0728902b01f4f987
SamirKhansa <samirkhansa01@gmail.com>
2025-09-30 19:40:52 +0300

    Did not add anything i was debugging and added some print statements in the future they will be deleted

--- Services/DocumentIntelligence.py
+++ Services/DocumentIntelligence.py
@@ -1,13 +1,18 @@
 import io
 
 def extract_text_with_read(DIClient, uploaded_file: bytes, locale: str | None = None) -> str:
-    file_stream = io.BytesIO(uploaded_file)
+    print("entering document intelligence")
+    try:
+        file_stream = io.BytesIO(uploaded_file)
 
-    poller = DIClient.begin_analyze_document(
-        model_id="prebuilt-read",
-        body=file_stream,
-        locale=locale,          
-    )
-    
-    result = poller.result()
-    return result.content or ""
+        poller = DIClient.begin_analyze_document(
+            model_id="prebuilt-read",
+            body=file_stream,
+            locale=locale,          
+        )
+        print("Problem is with Document Intelligence")
+        result = poller.result()
+        return result.content or ""
+    except Exception as e:
+        print("Error in document intellignece ",e)
+        return ""
