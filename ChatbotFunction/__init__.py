9c03cd0cc472154312eaa061050e65ff2130ce2a
SamirKhansa <samirkhansa01@gmail.com>
2025-09-30 21:36:07 +0300

    Cleaned the code for it to show which api is calling which function

--- ChatbotFunction/__init__.py
+++ ChatbotFunction/__init__.py
@@ -2,19 +2,27 @@ from keys import GPT4O_KEY,EMBEDDING_KEY, GPT4O_VERSION, EMBEDDING_VERSION, EMBE
 from config import get_client, get_CosmosDB, SearchClient1, SpeechClient, DIClient
 import azure.functions as func
 from helpers.ErrorHandling import SystemError
-from Services.OpenAiServices import SendReply
-from Services.AiSearchServices import DeletingDocument
+from Services.OpenAiServices import SendReply, ReplyToAdmin
 
-from Services.CosmosDbServices import PromoteDenoteUsers, DeleteUser
+
+
+from Services.CosmosDbServices import PromoteDenoteUsers, DeleteUser, RetreivingDocuments, ClearHistory
 from Controllers.Chat import ChatBot
-from Controllers.Admin import UploadingDocuments
-from requests_toolbelt.multipart import decoder
+from Controllers.Admin import UploadingDocuments, DeleteDocument
+
+
 
 from Controllers.Authentication import LogIn, SignUp
 
 
 ChatContainer = get_CosmosDB(container_name="Sessions", partitionKey="/sessionId")  
+
 UserContainer = get_CosmosDB(container_name="Users", partitionKey="/UserId")  
+
+DocumentContainer=get_CosmosDB(container_name="Documents", partitionKey="/documents")
+
+
+
 search_client=SearchClient1()
 speechClient=SpeechClient()
 
@@ -24,87 +32,41 @@ EmbeddingClient=get_client(KEY=EMBEDDING_KEY, VERSION=EMBEDDING_VERSION, ENDPOIN
 DIClients = DIClient()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    
-    print("0")
-    
     try:
-        print("1")
+       
         content_type = req.headers.get("content-type", "")
-        if "multipart/form-data" in content_type:
-            print("============================================================================")
-            body = req.get_body()
-            
-            print("3")
-            multipart_data = decoder.MultipartDecoder(body, content_type)
 
-            uploaded_file = None
-            resource = None
-            Type=None
-            UploadedBy=None
-            DocumentName=None
-
-            # Loop through each part
-            for part in multipart_data.parts:
-                content_disposition = part.headers.get(b'Content-Disposition', b'').decode()
-                if 'name="file"' in content_disposition:
-                    uploaded_file = part.content
-                if 'name="Resource"' in content_disposition:
-                    resource = part.content.decode()
-                if('name="Type"' in content_disposition):
-                    Type=part.content.decode()
-                if('name="UploadedBy"' in content_disposition):
-                    UploadedBy=part.content.decode()
-                if('name="name"' in content_disposition):
-                    DocumentName=part.content.decode()
-
-
-            if not uploaded_file:
-                return func.HttpResponse("No file uploaded", status_code=400)
-
-            if not resource:
-                resource = "Unknown Resource"
-            UploadingDocuments(uploaded_file, resource,Type,UploadedBy, DocumentName, DIClients, EmbeddingClient, search_client)
-            return SendReply("File Uploaded Sucessfully")
+        if "multipart/form-data" in content_type:  #Uploading a document
+            return SendReply(UploadingDocuments(req,content_type, DocumentContainer, DIClients, EmbeddingClient, search_client))
     
         req_body = req.get_json()
         route=req.route_params.get('route','')
         
-        print("route")
-        
         if(route=='Chat'):
             return ChatBot(req_body ,EmbeddingClient, GPT4oClient, speechClient, search_client, ChatContainer)
-        elif(route=="Client Uploading document/image"):
-            return
+        
         elif(route=="Promote" or route=="Denote"):
-            print("i am correct route ",route)
-            if(route=="Promote"):
-                Role="Admin"
-            else:
-                Role="User"
-            print("email will be printed soon")
-            email=req_body.get("email")
-            print("I am Role ",Role," Now email ",email)
-            Message=PromoteDenoteUsers( UserContainer=UserContainer, emails=email, Role=Role)
-            print("Now the message ",Message)
-            return SendReply(Message=Message)
+            return SendReply(PromoteDenoteUsers( UserContainer, req_body.get("email"), route))
         
         elif(route=="DeleteUser"):
             return SendReply(DeleteUser(UserContainer=UserContainer, email=req_body.get("email")))
-        elif(route=="Clear Conversation"):
-            return
+        
+        elif(route=="ViewDocument"):
+            return ReplyToAdmin(RetreivingDocuments(DocumentContainer, req_body.get("name"), req_body.get("resource")))
         
         elif(route=="LogIn"):
-            return LogIn(UserContainer, req_body, search_client)
+            return LogIn(UserContainer,DocumentContainer ,req_body, search_client)
+        
         elif(route=="SignUp"):
-            return SignUp(UserContainer, req_body)
+            return SendReply(SignUp(UserContainer, req_body))
         
         elif(route=="DeleteDocument"):
-            DeletingDocument(req_body=req_body, search_client=search_client)
-            return SendReply("Deletion is Sucessfull total number of chunks deleted is ")
+            return SendReply(DeleteDocument(req_body, DocumentContainer, search_client))
+        elif(route=="ClearHistory"):
+            return SendReply(ClearHistory(ChatContainer, req_body.get("SessionId")))
+        return SendReply("No message has returned")
         
         
-        elif(route=="Face Recognition"):
-            return
         
 
 
@@ -115,6 +77,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         
 
     except Exception as e:
+        print("Error ",e)
         return SystemError(e)
     
 
