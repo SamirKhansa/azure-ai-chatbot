50da34ce875844efdc2a1819516559e1ae4c3a3f
SamirKhansa <samirkhansa01@gmail.com>
2025-10-29 20:04:44 +0200

    Removed print statements

--- Controllers/Chat.py
+++ Controllers/Chat.py
@@ -16,8 +16,7 @@ from Services.SpeechAPI import text_to_speech, speech_to_text
 
 
 def ChatBot(req_body,EmbeddingClient, GPT4oClient, speechClient, search_client, ChatContainer):
-    
-    
+
     user_message = req_body.get("message")
     audio_base64 = req_body.get("audioBase64")
     session_id = req_body.get("sessionId")
@@ -36,32 +35,21 @@ def ChatBot(req_body,EmbeddingClient, GPT4oClient, speechClient, search_client, 
     if not session_id:
         session_id = CreateNewId()
 
-    print("I am the user Message ",user_message)
+    
     conversation, document_id = ConversationHistory(session_id=session_id, container=ChatContainer, user_message=user_message)
     
-    print("Problem is not in cosmos DB")
     query_embedding = TextEmbedding(EmbeddingClient=EmbeddingClient, message=user_message)
-    print("Problem is not in text embedding")
     
     results = RetreiveRelevantChunks(search_client=search_client,query_embedding=query_embedding, top_k=3)
-    print("Problem is not in AI Search")
-    
     
     retrieved_texts = [result["Content"] for result in results]
     
     context_prompt = "\n\n".join(retrieved_texts)
     
-
     ai_message = AiReply(client=GPT4oClient, conversation=conversation, context=context_prompt, tools=PromptForImageGeneration())
-    print("5555")
-    print("I am in the chat page")
-    print(ai_message["type"])
-    print("type was printed")
     
     if(ai_message["type"]!="image"):
-        print("666666")
         audio_base64=text_to_speech(ai_message["Content"], speechClient)
-        print("77777")
         ai_message2=AddingRefrencesToAiMessage(results=results, ai_message=ai_message["Content"])
         
         conversation.append({"role": "bot", "Content": ai_message2})
